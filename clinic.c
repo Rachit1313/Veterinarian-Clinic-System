5ea81f42f0795804ef5475a386f927408cbb8d46
Rachit1313 <58596563+Rachit1313@users.noreply.github.com>
2022-08-19 10:48:58 -0400

    Update clinic.c

--- clinic.c
+++ clinic.c
@@ -1,18 +1,3 @@
-/*
-*****************************************************************************
-                        Assignment 1 - Milestone 3
-Full Name  :Rachit Chawla
-Student ID#:162759211
-Email      :rchawla11@myseneca.ca
-Section    :ZDD
-
-Authenticity Declaration:
-I declare this submission is the result of my own work and has not been
-shared with any other student or 3rd party content provider. This submitted
-piece of work is entirely of my own creation.
-*****************************************************************************
-*/
-
 #define _CRT_SECURE_NO_WARNINGS
 
 #include <stdio.h>
